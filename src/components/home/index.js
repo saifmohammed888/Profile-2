96baacbb54bfd6ff674078b6a1feec58d1e55772
msaif001 <saifmohammed888@gmail.com>
2024-07-22 23:36:12 +0530

    fix: home

---
+++ src/components/home/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "./home.module.css"
+
+function Home() {
+  return (
+    <div className={`home_app ${style.home}`}> 
+        {/* Navbar */}
+        <div>
+        
+        </div>
+        <div>
+            {/* Content */}
+            <div></div>
+            {/* Image */}
+            <div></div>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
